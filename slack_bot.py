dbb021c7270c437dbc9e44db93efe52940da49cf
yukpiz <yukpiz@gmail.com>
2017-10-20 14:05:05 +0900

    Updated.

--- slack_bot.py
+++ slack_bot.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 def lambda_handler(event, context):
-    print("hogehgoe")
-    return { "statusCode": "200", "body": "hogehoge" }
+    print("-------------------------------")
+    print(event)
+    print("-------------------------------")
+    payload = {
+        "text": "hi?"
+    }
+    return { "statusCode": 200, "body": json.dumps(payload) }
